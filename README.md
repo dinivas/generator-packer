66a87ad26f5bcb77f292ef93184986b7874759c9
Rachid <ouattchidi@gmail.com>
2019-01-12 23:45:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-packer [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> Yeoman generator to scaffold packer project
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-packer using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-packer
+```
+
+Then generate your new project:
+
+```bash
+yo packer
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+Apache-2.0 © [Rachide](orachide.chidix.fr)
+
+
+[npm-image]: https://badge.fury.io/js/generator-packer.svg
+[npm-url]: https://npmjs.org/package/generator-packer
+[travis-image]: https://travis-ci.org/shepherd/generator-packer.svg?branch=master
+[travis-url]: https://travis-ci.org/shepherd/generator-packer
+[daviddm-image]: https://david-dm.org/shepherd/generator-packer.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/shepherd/generator-packer
---
+++ generators/app/index.js
@@ -0,0 +1,121 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const _ = require('lodash');
+const path = require('path');
+const mkdirp = require('mkdirp');
+
+function computeProjectName(name) {
+  name = _.kebabCase(name);
+  name = name.indexOf('packer-') === 0 ? name : 'packer-' + name;
+  return name;
+}
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+  }
+
+  initializing() {
+    this.props = {
+      projectName: this.options.projectName || this.appname,
+      imageName: this.options.imageName || ''
+    };
+  }
+
+  _askForProjectName() {
+    const done = this.async();
+    return this.prompt({
+      type: 'input',
+      name: 'projectName',
+      default: computeProjectName(path.basename(process.cwd())),
+      message: `What is your project name? (The generated project name will be in form generator-${chalk.green('projectName')}`,
+      store: true
+    }).then(answer => {
+      this.props.projectName = computeProjectName(answer.projectName);
+      done();
+    });
+  }
+
+  _askForImageName() {
+    const done = this.async();
+    return this.prompt({
+      type: 'input',
+      name: 'imageName',
+      default: '',
+      message: 'What is the name of the Image to build?',
+      store: true
+    }).then(answer => {
+      this.props.imageName = answer.imageName;
+      done();
+    });
+  }
+
+  get prompting() {
+    return {
+      askForProjectName: this._askForProjectName,
+      askForImageName: this._askForImageName
+    }
+  }
+
+  configuring() {
+
+  }
+
+  default () {
+    if (path.basename(this.destinationPath()) !== this.props.projectName) {
+      this.log(
+        `Your generator must be inside a folder named ${
+          this.props.projectName
+        }\nI'll automatically create this folder.`
+      );
+      mkdirp(this.props.projectName);
+      this.destinationRoot(this.destinationPath(this.props.projectName));
+    }
+  }
+
+  writing() {
+    // Copy Packer main template file
+    this.fs.copyTpl(
+      this.templatePath('template.json.ejs'),
+      this.destinationPath('template.json'), {
+        projectName: this.props.projectName,
+        imageName: this.props.imageName
+      }
+    );
+
+    // Copy Ansible Playbook file
+    this.fs.copyTpl(this.templatePath('playbook.yml.ejs'), this.destinationPath('playbook.yml'), {
+      projectName: this.props.projectName,
+      imageName: this.props.imageName
+    });
+
+    // Copy Ansible Galaxy requirements file
+    this.fs.copy(
+      this.templatePath('requirements.yml'),
+      this.destinationPath('requirements.yml')
+    );
+
+    // Copy .gitignore file
+    this.fs.copy(this.templatePath('.gitignore'), this.destinationPath('.gitignore'));
+
+    this.fs.copyTpl(
+      this.templatePath('README.md.ejs'),
+      this.destinationPath('README.md'), {
+        projectName: this.props.projectName
+      }
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('Makefile.ejs'),
+      this.destinationPath('Makefile')
+    );
+  }
+
+  install() {}
+
+  end() {
+    this.log(`Thanks for using ${chalk.cyan('generator-packer')}.`);
+  }
+};
